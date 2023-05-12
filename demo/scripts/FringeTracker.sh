e9da4a3fdf485c6e22f832a6a8a3e23c9cffdfec
Anders M. Jorgensen <andersmjorgensen@gmail.com>
2023-05-12 07:27:52 -0600

    Changed power spectrum UDP port. changed script to use Debug version of power spectrum viewer.

--- demo/scripts/FringeTracker.sh
+++ demo/scripts/FringeTracker.sh
@@ -5,5 +5,5 @@ export LD_LIBRARY_PATH=/opt/amjCom/lib
 ../../FringeTracker/src/tracker --baseline baseline1 200 1 2.5 4 5 1 \
 	  --baseline baseline2 250 1.5 3 4 5 1 \
 	  --receiver-phasors 127.0.0.1:27001 \
-	  --sender-pspec 127.0.0.1:27004 1000 \
+	  --sender-pspec 127.0.0.1:27006 1000 \
 	  --sender-movements 127.0.0.1:27005
