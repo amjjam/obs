48b16ee91a00557605a9f6f66ede8ad229ed7d71
Anders M. Jorgensen <andersmjorgensen@gmail.com>
2023-06-09 04:11:10 -0600

    Updated simulator parameters

--- demo/scripts/Simulator.sh
+++ demo/scripts/Simulator.sh
@@ -4,8 +4,8 @@ export LD_LIBRARY_PATH=/opt/amjCom/lib
 
 ../../Simulator/src/simulator \
     --sender-phasors 127.0.0.1:27001 \
-    --sender-phasors 127.0.0.1:27002 \
-    --baseline baseline1 2 1 200 1 2.5 4 5 1 \
-    --baseline baseline2 3 2 250 1.5 3 4 5 2
-
-	   
+    --sender-phasors2 127.0.0.1:27002 100\
+    --receiver-delaylines 127.0.0.1:27005 \
+    --baseline baseline1 1 2 200 1 2.5 1 1 1 \
+    --baseline baseline2 2 3 250 1.5 3 1 1 2 \
+    --externaldelaymodel 2 square 50 10
