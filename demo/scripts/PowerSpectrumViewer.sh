b5a8aa8436f52c4c5e7eb56fd69976e48cc1c1e6
Anders M. Jorgensen <andersmjorgensen@gmail.com>
2023-06-09 04:10:35 -0600

    nothing

--- demo/scripts/PowerSpectrumViewer.sh
+++ demo/scripts/PowerSpectrumViewer.sh
@@ -2,5 +2,5 @@
 
 export LD_LIBRARY_PATH=/opt/amjCom/lib:/opt/QCustomPlot/lib
 
-../../PowerSpectrumViewer/build-PowerSpectrumViewer-Desktop-Debug/PowerSpectrumViewer \\
+../../PowerSpectrumViewer/build-PowerSpectrumViewer-Desktop-Debug/PowerSpectrumViewer \
 --receiver-psec 127.0.0.1:27006
