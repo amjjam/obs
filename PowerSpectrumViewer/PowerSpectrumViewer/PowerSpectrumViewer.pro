fef716f99bbcd01610074cad102b4d4f331eb07f
Anders M. Jorgensen <andersmjorgensen@gmail.com>
2024-02-13 08:10:58 -0700

    Modifications for build and install

--- PowerSpectrumViewer/PowerSpectrumViewer/PowerSpectrumViewer.pro
+++ PowerSpectrumViewer/PowerSpectrumViewer/PowerSpectrumViewer.pro
@@ -1,6 +1,16 @@
-QT       += core gui network
 
+QT       += core gui network
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+isEmpty(prefix){
+  prefix=/opt/obs
+}
+isEmpty(INC){
+  INC=/opt/amj/include /opt/QCustomPlot/include
+}
+isEmpty(LIB){
+  LIB=-L/opt/amj/lib -L/opt/QCustomPlot/lib
+}
 
 CONFIG += c++11 debug
 
@@ -8,39 +18,19 @@ CONFIG += c++11 debug
 # In order to do so, uncomment the following line.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
-SOURCES += \
-    main.C \
-    powerspectrumviewer.C \
-    ../../shared/src/PowerSpectrum.C
+SOURCES += main.C powerspectrumviewer.C ../../shared/src/PowerSpectrum.C
 
-HEADERS += \
-    powerspectrumviewer.H
+HEADERS += powerspectrumviewer.H
 
-FORMS += \
-    powerspectrumviewer.ui
+FORMS += powerspectrumviewer.ui
 
+LIBS += $${LIB} -lqcustomplot -lamjCom
+
+INCLUDEPATH += $${INC}
+        
 # Default rules for deployment.
-qnx: target.path = /tmp/$${TARGET}/bin
-else: unix:!android: target.path = /opt/$${TARGET}/bin
+target.path = $${prefix}/bin
 !isEmpty(target.path): INSTALLS += target
 
 # Tell the qcustomplot header that it will be used as library:
 DEFINES += QCUSTOMPLOT_USE_LIBRARY
-
-# Link with debug version of qcustomplot if compiling in debug mode, else with release library:
-#CONFIG(debug, release|debug) {
-#  win32:QCPLIB = qcustomplotd2
-#  else: QCPLIB = qcustomplotd
-#} else {
-#  win32:QCPLIB = qcustomplot2
-#  else: QCPLIB = qcustomplot
-#}
-QCPLIB=qcustomplot
-
-LIBS += -L/opt/QCustomPlot/lib/ -l$$QCPLIB
-INCLUDEPATH +=/opt/QCustomPlot/include
-LD_LIBRARY_PATH +=/opt/QCustomPlot/lib
-
-LIBS += -L/opt/amjCom/lib -lamjCom
-INCLUDEPATH += /opt/amjCom/include
-LD_LIBRARY_PATH +=/opt/amjCom/lib
--- PowerSpectrumViewer/PowerSpectrumViewer/powerspectrumviewer.C
+++ PowerSpectrumViewer/PowerSpectrumViewer/powerspectrumviewer.C
@@ -13,12 +13,7 @@ PowerSpectrumViewer::PowerSpectrumViewer(QWidget *parent)
   socket->bind(QHostAddress::LocalHost,port);
   connect(socket,&QUdpSocket::readyRead,this,&PowerSpectrumViewer::receive);
   //std::cout << "started" << std::endl;
-  ui->widget->addGraph();
-  ui->widget->graph(0)->setPen(QPen(Qt::red));
-  ui->widget->addGraph();
-  ui->widget->graph(1)->setPen(QPen(Qt::green));
-  ui->widget->addGraph();
-  ui->widget->graph(2)->setPen(QPen(Qt::blue));
+  _colors={Qt::red,Qt::green,Qt::blue,Qt::magenta,Qt::cyan};
 }
 
 PowerSpectrumViewer::~PowerSpectrumViewer(){
@@ -31,8 +26,34 @@ void PowerSpectrumViewer::receive(){
       packet.clear();
       packet.resize(datagram.data().size());
       memcpy(packet._data(),datagram.data().data(),datagram.data().size());
+      std::vector<std::string> names;
       std::vector<PowerSpectrum> ps;
+      packet >> names;
       packet >> ps;
+
+      if(names!=_names){ // Plot needs to be reconfigured
+        _names=names;
+        std::cout << "reconfigure" << std::endl;
+        ui->plot->plotLayout()->clear();
+        QCPAxisRect *r=new QCPAxisRect(ui->plot);
+        ui->plot->plotLayout()->addElement(0,0,r);
+        for(unsigned int i=0;i<_names.size();i++){
+          ui->plot->addGraph();
+          ui->plot->graph(i)->setPen(_colors[i%_colors.size()]);
+          ui->plot->graph(i)->setName(QString::fromStdString(_names[i]));
+          ui->plot->graph(i)->setKeyAxis(ui->plot->axisRect(0)->axis(QCPAxis::atBottom,0));
+          ui->plot->graph(i)->setValueAxis(ui->plot->axisRect(0)->axis(QCPAxis::atLeft,0));
+        }
+
+        ui->plot->legend=new QCPLegend();
+        ui->plot->legend->setFillOrder(QCPLayoutGrid::foColumnsFirst);
+        ui->plot->legend->setWrap(5);
+        ui->plot->plotLayout()->addElement(ui->plot->axisRectCount(),0,ui->plot->legend);
+        ui->plot->plotLayout()->setRowStretchFactor(ui->plot->axisRectCount(),0.00001);
+        for(int i=0;i<ui->plot->graphCount();i++)
+          ui->plot->graph(i)->addToLegend();
+        ui->plot->legend->setVisible(true);
+      }
 
       for(unsigned int i=0;i<ps.size();i++){
         QVector<double> d(ps[i].delays().size());
@@ -41,12 +62,13 @@ void PowerSpectrumViewer::receive(){
             d[j]=ps[i].delays()[j];
             p[j]=ps[i].power()[j];
         }
-        ui->widget->graph(i)->setData(d,p);
+        std::cout << "setdata" << std::endl;
+        ui->plot->graph(i)->setData(d,p);
       }
-      ui->widget->xAxis->setRange(0,1024);
-      ui->widget->yAxis->setRange(0,50);
-      ui->widget->rescaleAxes();
-      ui->widget->replot();
+      ui->plot->xAxis->setRange(0,1024);
+      ui->plot->yAxis->setRange(0,50);
+      ui->plot->rescaleAxes();
+      ui->plot->replot();
     }
 }
 
