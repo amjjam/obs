7e5eeceb00a13612b8c9b5455b6d7c6d8160b4eb
Anders M. Jorgensen <andersmjorgensen@gmail.com>
2024-08-15 11:59:14 -0600

    A third demo script directory

---
+++ demo/demo3/scripts/Simulator.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=/opt/amjCom/lib:/opt/amj/lib
+
+../../../Simulator/src/Simulator \
+    --sender-frames /frames:2:10000 \
+    --sender-frames2 /frameviewer:2:10000 100\
+    --receiver-delaylines 127.0.0.1:27003 \
+    --framesize 64 64 \
+    --baseline baseline1 1 2 200 1 2.5 1 1 1 \
+    --baseline baseline2 2 3 250 1.5 3 1 1 2 \
+    --externaldelaymodel 1 atm 2 10 \
+    --externaldelaymodel 2 atm 2 10 \
+    --externaldelaymodel 3 atm 2 10 \
+    --simlog /tmp/test8.simlog
